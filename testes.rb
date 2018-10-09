a5b538ca51a1cc85794459733d90b8cbc518dbc0
KleberDevMen <klebersubcontas@gmail.com>
2018-10-09 09:51:12 -0300

    Initial commit

---
+++ testes.rb
@@ -0,0 +1,48 @@
+# TEORIA ----------------
+# site: http://ruby-doc.org/
+# METODO: dentro de um escopo (de um objeto String, numeber...)
+# FUNCAO: fica voando, é uma DEF
+
+# interpretador linha a linha: irb
+
+# OPERADORES -----------------------
+
+#puts 175 == 175
+#puts "175"==175
+#puts "175".to_i == 175
+#puts
+# % = resto
+# +=, *=, /=
+ 
+
+# REPETICAO -----------------------
+
+#for contador in 1..3
+#    puts "Contando " + contador.to_s
+#end
+
+# while quer_jogar
+#    joga nome, dificuldade
+#end
+
+# ARRAY -----------------------
+
+#chutes = [1, 2, 3, 4, 5]
+#chutes[3] = 444
+#puts chutes[0]
+#puts chutes[1]
+#puts chutes[2]
+#puts chutes[3]
+#puts chutes[4]
+#chutes << 2 //coloca na ultima posicao
+#chutes.size //tamanho
+
+# CARACTERES -----------------------
+# //remove caracteres e quebra de linha na direita e esquerda
+#nome = gets.strip
+#nome = "kleber ".strip
+#puts nome
+# //pega apenas o conteudo texte das variaveis
+#puts "#{nome} tem #{nome.size} caracteres"
+
+
